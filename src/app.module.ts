e535ab9451d943401ae491a1e39ddaf303c570a5
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2025-04-22 16:59:21 -0300

    Implementar variáveis de ambientes para segredos

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,17 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { FormularioIdeiasModule } from './formulario-ideias/formulario-ideias.module';
+import { env } from 'node:process';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'pi2025',
-      database: 'pi2025',
+      host: env.PI2025_HOST ?? 'localhost',
+      port: Number(env.PI2025_PORT) ?? 3306,
+      username: env.PI2025_USERNAME ?? '',
+      password: env.PI2025_PASSWORD ?? '',
+      database: env.PI2025_DATABASE ?? 'pi2025',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
