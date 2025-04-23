4d1ac9b17b515604a6e49ea3b1030a984b7b2e82
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2025-04-23 20:29:33 -0300

    Adiciona suporte a HTTPS

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { env } from 'node:process';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const keyFile = process.env.PI2025_KEY_FILE;
+  const certFile = process.env.PI2025_CERT_FILE;
+
+  const app = await NestFactory.create(AppModule,
+    {
+      httpsOptions: keyFile != null && certFile != null ? {
+      key: fs.readFileSync(keyFile),
+      cert: fs.readFileSync(certFile),
+      } : undefined
+    }
+  );
 
   // Requisições permitidas de qualquer fonte
   app.enableCors({
